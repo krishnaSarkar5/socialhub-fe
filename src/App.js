2f64615db0be5e9021b8bfbff061c883a002eb8a
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-07-01 22:41:52 +0530

    added redux setup,added theme setup, added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <h1>Hi</h1>
+    <div>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>  
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
